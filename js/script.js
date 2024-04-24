d901c8969c6c5882727b36347162efc74e727929
KarbitsCode <adipramana623@gmail.com>
2024-04-24 15:52:18 +0800

    Add regex validation (test)

--- js/script.js
+++ js/script.js
@@ -13,39 +13,43 @@ const doConvert = () => {
         if (!(isNaN(celcius))) {
             let result = ((celcius * (9/5)) + 32)
             let resultFormula = `(${celcius}°C * (9/5)) + 32 = ${result}°F`
-            console.log(resultFormula)
+            console.log("INFO:", resultFormula)
             resultText.value = result
             formulaText.value = resultFormula
             explanationText.innerHTML = `
-            <div class="explanation-text">Cara Kerja Konversi Suhu</div>
+            <div class="explanation-text">Cara Kerja Konversi Suhu Celcius (°C)</div>
             <div class="normal-text">Suhu <big><i>S</i></big> dalam derajat Fahrenheit (&deg;F) sama dengan suhu <big><i>S</i></big> dalam derajat Celcius (&deg;C) kali <big>9/5</big> tambah <big>32</big>.</div>
             <br>
             <div class="normal-text"><big><i>S</i></big><sub>(&deg;F)</sub> = (<big><i>S</i></big><sub>(&deg;C)</sub> &times; <big>9/5</big>) &plus; <big>32</big></div>`
+            explanationText.style.display = "inline"
         } else {
-            resultText.value = "Error: Mohon masukan angka"
+            resultText.value = "ERROR: Mohon masukan angka"
             formulaText.value = ""
-            throw "NaN"
+            explanationText.style.display = "none"
+            console.error("ERROR: Mohon masukan angka")
         }
     } else if (inputLabel.textContent === "Fahrenheit (°F)") {
         let fahrenheit = parseFloat(inputText.value)
         if (!(isNaN(fahrenheit))) {
             let result = ((fahrenheit - 32) * (5/9))
             let resultFormula = `(${fahrenheit}°C - 32) * (5/9) = ${result}°F`
-            console.log(resultFormula)
+            console.log("INFO:", resultFormula)
             resultText.value = result
             formulaText.value = resultFormula
             explanationText.innerHTML = `
-            <div class="explanation-text">Cara Kerja Konversi Suhu</div>
+            <div class="explanation-text">Cara Kerja Konversi Suhu Fahrenheit (°F)</div>
             <div class="normal-text">Suhu <big><i>S</i></big> dalam derajat Celcius (&deg;C) sama dengan suhu <big><i>S</i></big> dalam derajat Fahrenheit (&deg;F) kurang <big>32</big> dikali <big>5/9</big></div>
             <br>
             <div class="normal-text"><big><i>S</i></big><sub>(&deg;C)</sub> = (<big><i>S</i></big><sub>(&deg;F)</sub> &minus; <big>32</big>) &times; <big>5/9</big></div>`
+            explanationText.style.display = "inline"
         } else {
-            resultText.value = "Error: Mohon masukan angka"
+            resultText.value = "ERROR: Mohon masukan angka"
             formulaText.value = ""
-            throw "NaN"
+            explanationText.style.display = "none"
+            console.error("ERROR: Mohon masukan angka")
         }
     } else {
-        throw "Unknown state"
+        console.error("ERROR: Unknown state")
     }
 }
 
@@ -53,13 +57,13 @@ const doReverse = () => {
     if (curState1.textContent === "Celcius (°C)") {
         curState1.textContent = "Fahrenheit (°F)"
         curState2.textContent = "Celcius (°C)"
-        console.log("Label ditukar", curState1.textContent, "dengan", curState2.textContent)
+        console.log("INFO: Label ditukar", curState1.textContent, "dengan", curState2.textContent)
     } else if (curState1.textContent === "Fahrenheit (°F)") {
         curState1.textContent = "Celcius (°C)"
         curState2.textContent = "Fahrenheit (°F)"
-        console.log("Label ditukar", curState1.textContent, "dengan", curState2.textContent)
+        console.log("INFO: Label ditukar", curState1.textContent, "dengan", curState2.textContent)
     } else {
-        throw "Unknown state"
+        console.error("ERROR: Unknown state")
     }
 }
 
@@ -68,7 +72,7 @@ const doReset = () => {
     resultText.value = ""
     formulaText.value = ""
     explanationText.style.display = "none"
-    console.log("Text area dibersihkan")
+    console.log("INFO: Text area dibersihkan")
 }
 
 inputText.addEventListener("keypress", (event) => {
@@ -76,4 +80,15 @@ inputText.addEventListener("keypress", (event) => {
         event.preventDefault()
         doConvert()
     }
+})
+
+inputText.addEventListener("input", (state) => {
+    let regex = /^[0-9.]+$/
+    if (!(regex.test(state.data))) {
+        console.warn("WARNING: Input bukan angka:", state.data)
+    }
+})
+
+window.addEventListener("load", () => {
+    doReset()
 })
